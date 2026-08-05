f524aa2ed56fbf78b625c5a90dddd3fc6e61980b
npgmanu <manuel.rozehnal@gmail.com>
2026-08-05 16:48:59 -0600

    Update D1 database configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,8 +18,8 @@ const localBindingConfig = {
     ? [
         {
           binding: d1,
-          database_name: "site-creator-d1",
-          database_id: SITE_CREATOR_PLACEHOLDER_DATABASE_ID,
+          database_name: "sticker-book-db",
+          database_id: "6ccff695-3ea5-4c95-b7f8-d54da68a714d",
         },
       ]
     : [],
